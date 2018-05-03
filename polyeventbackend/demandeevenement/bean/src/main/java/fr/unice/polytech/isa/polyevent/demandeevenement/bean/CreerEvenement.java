55d6785d396f9f7102808290bf222bd7e3efeaa4
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-05-03 10:22:20 +0200

    Séparation de demandeEvenement en un bean et un webservice.

---
+++ polyeventbackend/demandeevenement/bean/src/main/java/fr/unice/polytech/isa/polyevent/demandeevenement/bean/CreerEvenement.java
@@ -0,0 +1,18 @@
+package fr.unice.polytech.isa.polyevent.demandeevenement.bean;
+
+import fr.unice.polytech.isa.polyevent.entities.DemandePrestataire;
+import fr.unice.polytech.isa.polyevent.entities.DemandeReservationSalle;
+import fr.unice.polytech.isa.polyevent.entities.Token;
+
+import javax.ejb.Local;
+import java.util.Date;
+import java.util.List;
+
+@Local
+public interface CreerEvenement
+{
+    void demanderCreationEvenement(Token token, String nom,
+                                   Date dateDebut, Date dateFin,
+                                   List<DemandeReservationSalle> demandeReservationSalles,
+                                   List<DemandePrestataire> demandePrestataires);
+}
---
+++ polyeventbackend/demandeevenement/webservice/src/main/java/fr/unice/polytech/isa/polyevent/demandeevenement/webservice/DemandeEvenement.java
@@ -0,0 +1,26 @@
+package fr.unice.polytech.isa.polyevent.demandeevenement.webservice;
+
+import fr.unice.polytech.isa.polyevent.demandeevenement.bean.CreateurEvenement;
+import fr.unice.polytech.isa.polyevent.entities.DemandePrestataire;
+import fr.unice.polytech.isa.polyevent.entities.DemandeReservationSalle;
+import fr.unice.polytech.isa.polyevent.entities.Token;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import java.util.Date;
+import java.util.List;
+
+@WebService(targetNamespace = "http://www.polytech.unice.fr/si/4a/isa/polyevent/demandeEvenement")
+@Stateless(name = "DemandeEvenementWS")
+public class DemandeEvenement implements DemanderEvenement
+{
+    @EJB
+    private CreateurEvenement createurEvenement;
+
+    @Override
+    public void demanderCreationEvenement(Token token, String nom, Date dateDebut, Date dateFin, List<DemandeReservationSalle> demandeReservationSalles, List<DemandePrestataire> demandePrestataires)
+    {
+        createurEvenement.demanderCreationEvenement(token, nom, dateDebut, dateFin, demandeReservationSalles, demandePrestataires);
+    }
+}
