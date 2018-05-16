44521b0a8da41665a0d5a735f5fe97688fcc8623
FlorianLehmann <Lehmann.florian@yandex.com>
2018-05-16 17:43:23 +0200

    change jenkins params

--- jenkins.sh
+++ jenkins.sh
@@ -7,7 +7,7 @@ echo "change the in polyeventbackend/pom.xml to your own artifactory name"
 echo "------> <name>5c3c6f24472b-snapshots</name>"
 
 echo "User: florian"
-echo "Password: florian123"
+echo "Password: d62317724245475eb12d64a776810935"
 
 sleep 2
 
