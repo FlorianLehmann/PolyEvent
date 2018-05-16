24fbd099dc3ba1a5643f25148675f725986d407e
FlorianLehmann <Lehmann.florian@yandex.com>
2018-05-16 18:20:54 +0200

    fix escape character

--- jenkins.sh
+++ jenkins.sh
@@ -11,5 +11,5 @@ echo "Password: d62317724245475eb12d64a776810935"
 
 sleep 2
 
-docker run --user root -p 8080:8080 -p 50000:50000 -v $PWD/jenkins_data/jenkins_blue_ocean:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean_mod
+docker run --user root -p 8080:8080 -p 50000:50000 -v "$PWD"/jenkins_data/jenkins_blue_ocean:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean_mod
 
